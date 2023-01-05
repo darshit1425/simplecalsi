fd168663a6aa3e8a68357e25510d29e77d847a0f
HP <darshitmendapara1425@gmail.com>
2023-01-05 10:47:35 +0530

    maths calulator

---
+++ android/app/src/main/kotlin/com/example/simplecalsi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simplecalsi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:simplecalsi/Homepage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Cal(),
+    ),
+  );
+}
